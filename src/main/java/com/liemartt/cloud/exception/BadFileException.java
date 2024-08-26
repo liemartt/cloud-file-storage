141a17ed6e586cb808c32d626d864a9edba1ca41
Artem_Limonov <liemartt@yandex.ru>
2024-08-26 15:18:18 +0300

    feat: add folder methods to StorageService

---
+++ src/main/java/com/liemartt/cloud/exception/BadFileException.java
@@ -0,0 +1,6 @@
+package com.liemartt.cloud.exception;
+
+public class BadFileException extends RuntimeException {
+    public BadFileException(String message) {
+    }
+}
--- src/main/java/com/liemartt/cloud/service/MinIOStorageService.java
+++ src/main/java/com/liemartt/cloud/service/MinIOStorageService.java
@@ -1,11 +1,14 @@
 package com.liemartt.cloud.service;
 
+import com.liemartt.cloud.exception.BadFileException;
 import io.minio.*;
+import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -25,7 +28,7 @@ public class MinIOStorageService {
                     .contentType(file.getContentType())
                     .build());
         } catch (IOException e) {
-            throw new RuntimeException(e); //TODO bad file
+            throw new BadFileException(e.getMessage());
         }
     }
     
@@ -47,10 +50,85 @@ public class MinIOStorageService {
                         .object(pathToFile + name)
                         .build())
                 .build());
+        
         minioClient.removeObject(RemoveObjectArgs.builder()
                 .bucket(bucketName)
                 .object(pathToFile + name)
                 .build());
     }
     
+    @SneakyThrows
+    public void createFolder(String path, String name) {
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(path + name + "/")
+                        .stream(new ByteArrayInputStream(new byte[]{}), 0, -1)
+                        .build());
+    }
+    
+    @SneakyThrows
+    public void uploadFolder(String path, MultipartFile[] files) {
+        for (MultipartFile file : files) {
+            String fileName = file.getOriginalFilename();
+            
+            try (InputStream inputStream = file.getInputStream()) {
+                minioClient.putObject(
+                        PutObjectArgs.builder()
+                                .bucket(bucketName)
+                                .object(path+fileName)
+                                .stream(inputStream, file.getSize(), -1)
+                                .contentType(file.getContentType())
+                                .build());
+            }
+        }
+    }
+    
+    @SneakyThrows
+    public void renameFolder(String path, String folderName, String newFolderName) {
+        Iterable<Result<Item>> results = minioClient.listObjects(ListObjectsArgs.builder()
+                .bucket(bucketName)
+                .prefix(path + folderName)
+                .recursive(true)
+                .build());
+        
+        for (Result<Item> result : results) {
+            Item item = result.get();
+            String objectName = item.objectName();
+            String newObjectName = objectName.replaceFirst(folderName, newFolderName);
+            
+            minioClient.copyObject(CopyObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(newObjectName)
+                    .source(CopySource.builder()
+                            .bucket(bucketName)
+                            .object(objectName)
+                            .build())
+                    .build());
+            
+            minioClient.removeObject(RemoveObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(objectName)
+                    .build());
+        }
+    }
+    
+    @SneakyThrows
+    public void deleteFolder(String path, String folderName) {
+        Iterable<Result<Item>> results = minioClient.listObjects(ListObjectsArgs.builder()
+                .bucket(bucketName)
+                .prefix(path + folderName)
+                .recursive(true)
+                .build());
+        
+        for (Result<Item> result : results) {
+            Item item = result.get();
+            String objectName = item.objectName();
+            
+            minioClient.removeObject(RemoveObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(objectName)
+                    .build());
+        }
+    }
 }
