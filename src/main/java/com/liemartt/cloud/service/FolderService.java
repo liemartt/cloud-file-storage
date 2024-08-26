fd59307dd6bb18855b6aaef734c150091b34510b
Artem_Limonov <liemartt@yandex.ru>
2024-08-26 16:41:08 +0300

    refactor: divide MinIOStorageService into FileService and FolderService

--- src/main/java/com/liemartt/cloud/service/FolderService.java
+++ src/main/java/com/liemartt/cloud/service/FolderService.java
@@ -1,6 +1,5 @@
 package com.liemartt.cloud.service;
 
-import com.liemartt.cloud.exception.BadFileException;
 import io.minio.*;
 import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
@@ -9,53 +8,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 @Service
 @RequiredArgsConstructor
-public class MinIOStorageService {
+public class FolderService extends MinioService {
     private final MinioClient minioClient;
-    private final static String bucketName = "bucket";
-    
-    @SneakyThrows
-    public void uploadFile(String pathToFile, MultipartFile file) {
-        try (InputStream inputStream = file.getInputStream()) {
-            minioClient.putObject(PutObjectArgs.builder()
-                    .bucket(bucketName)
-                    .object(pathToFile + file.getOriginalFilename())
-                    .stream(inputStream, file.getSize(), -1)
-                    .contentType(file.getContentType())
-                    .build());
-        } catch (IOException e) {
-            throw new BadFileException(e.getMessage());
-        }
-    }
-    
-    @SneakyThrows
-    public void deleteFile(String filePath) {
-        minioClient.removeObject(RemoveObjectArgs.builder()
-                .bucket(bucketName)
-                .object(filePath)
-                .build());
-    }
-    
-    @SneakyThrows
-    public void renameFile(String pathToFile, String name, String newName) {
-        minioClient.copyObject(CopyObjectArgs.builder()
-                .bucket(bucketName)
-                .object(pathToFile + newName)
-                .source(CopySource.builder()
-                        .bucket(bucketName)
-                        .object(pathToFile + name)
-                        .build())
-                .build());
-        
-        minioClient.removeObject(RemoveObjectArgs.builder()
-                .bucket(bucketName)
-                .object(pathToFile + name)
-                .build());
-    }
     
     @SneakyThrows
     public void createFolder(String path, String name) {
@@ -76,7 +34,7 @@ public class MinIOStorageService {
                 minioClient.putObject(
                         PutObjectArgs.builder()
                                 .bucket(bucketName)
-                                .object(path+fileName)
+                                .object(path + fileName)
                                 .stream(inputStream, file.getSize(), -1)
                                 .contentType(file.getContentType())
                                 .build());
